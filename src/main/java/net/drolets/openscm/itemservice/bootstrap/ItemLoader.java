98b1836e0a8705e5dc96b4b3d39ec075c47541f5
tjd <noreplay@acme.com>
2020-07-31 15:25:11 -0400

    add brief explaination of OpenSCM concept

---
+++ src/main/java/net/drolets/openscm/itemservice/bootstrap/ItemLoader.java
@@ -0,0 +1,8 @@
+package net.drolets.openscm.itemservice.bootstrap;
+
+/**
+ * Created by tjd 7/27/20
+ */
+
+public class ItemLoader {
+}
---
+++ src/main/java/net/drolets/openscm/itemservice/repositories/ItemRepository.java
@@ -0,0 +1,8 @@
+package net.drolets.openscm.itemservice.repositories;
+
+/**
+ * Created by tjd 7/27/20
+ */
+
+public interface ItemRepository {
+}
