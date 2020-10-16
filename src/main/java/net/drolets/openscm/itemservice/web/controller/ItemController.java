9e235df4dd3d0b48752b403cb51a4fdb6130d8e7
tjd <noreplay@acme.com>
2020-10-16 14:14:46 -0400

    fix issue to avoid CORS problem on front end dev

--- src/main/java/net/drolets/openscm/itemservice/web/controller/ItemController.java
+++ src/main/java/net/drolets/openscm/itemservice/web/controller/ItemController.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 
 @RequestMapping("/api/v1/items")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemController {
 
     private final ItemService itemService;
